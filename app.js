15ed0e16ea2025c34f1eaca123dce753bf859fbd
Enyel Liranzo <xardimodschannel@gmail.com>
2024-03-23 22:03:02 -0400

    feat: adding new routes

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import { corsMiddleware } from './middleware/cors.js';
+import products from "./models/schema/products.json" assert { type: "json" };
+import vendors from "./models/schema/vendors.json" assert { type: "json" };
+import users from "./models/schema/users.json" assert { type: "json" };
 
 const app = express();
 const port = process.env.PORT ?? 3000;
@@ -12,6 +15,18 @@ app.get('/', (req, res) => {
   res.json({
     message: 'Hello World!'
   });
+})
+
+app.get('/products', (req, res) => {
+  res.json(products)
+})
+
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+app.get('/vendors', (req, res) => {
+  res.json(vendors)
 })
 
 app.listen(port, () => {
