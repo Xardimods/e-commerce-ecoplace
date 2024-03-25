3b4e528ac942e392778496f0d41c6d9d1866a4e9
Enyel Liranzo <xardimodschannel@gmail.com>
2024-03-25 18:06:11 -0400

    update: changing new routes

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { corsMiddleware } from './middleware/cors.js';
-import products from "./models/schema/products.json" assert { type: "json" };
+// import products from "./models/schema/products.json" assert { type: "json" };
 import vendors from "./models/schema/vendors.json" assert { type: "json" };
 import users from "./models/schema/users.json" assert { type: "json" };
 import { bbddConection } from './models/config/conection.js';
+import { ProductsRouter } from './routes/products.js';
 
 const app = express();
 const port = process.env.PORT ?? 3000;
@@ -12,29 +13,12 @@ app.disable('x-powered-by');
 app.use(corsMiddleware());
 app.use(express.json());
 
+app.use('/products', ProductsRouter);
+
 app.get('/', (req, res) => {
   res.json({
     message: 'Hello World!'
   });
-});
-
-app.get('/products', (req, res) => {
-  res.json(products)
-});
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  const product = products.products.find(product => product._id === parseInt(id));
-  if (!product) res.status(404).json({ message: "Not found." });
-  res.json(product);
-});
-
-app.get('/users', (req, res) => {
-  res.json(users)
-});
-
-app.get('/vendors', (req, res) => {
-  res.json(vendors)
 });
 
 app.listen(port, () => {
