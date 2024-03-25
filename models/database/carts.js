4ac6082486ec419adc46c676393cfb246339e4d0
Enyel Liranzo <xardimodschannel@gmail.com>
2024-03-25 15:23:43 -0400

    update: changing all files to ES Modules system

--- models/database/carts.js
+++ models/database/carts.js
@@ -1,21 +1,25 @@
-const mongoose = require('mongoose')
+import mongoose from 'mongoose';
 
 const cartSchema = new mongoose.Schema({
-  User: { 
-  type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  User: {
+    type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true
+  },
   items: [{
-    product: { 
-	type: mongoose.Schema.Types.ObjectId, 
-	ref: 'Product', 
-	required: true },
-    quantity: { 
-	type: Number, 
-	required: true, 
-	default: 1 }
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 1
+    }
   }],
-  createdAt: { 
-  type: Date, 
-  default: Date.now },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
 });
 
 cartSchema.virtual("id").get(function () {
--- models/database/roles.js
+++ models/database/roles.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from 'mongoose';
 
 const roleSchema = mongoose.Schema({
   roleName: {
