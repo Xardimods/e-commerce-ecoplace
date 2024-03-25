7aaab047a0e90bf8cbb634aca0d419e87fdb22ab
Enyel Liranzo <xardimodschannel@gmail.com>
2024-03-25 18:05:56 -0400

    feat: adding products routes

---
+++ routes/products.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { ProductsController } from '../controllers/products.js';
+
+export const ProductsRouter = Router();
+
+ProductsRouter.get('/', ProductsController.getAll);
+ProductsRouter.get('/:id', ProductsController.getById);
+
+ProductsRouter.post('/', ProductsController.createProduct);
+
+ProductsRouter.patch('/:id', ProductsController.updateProduct)
+
+ProductsRouter.delete('/:id', ProductsController.deleteProduct)
