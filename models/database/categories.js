d4d7acf8e5cd5144c8757e3a025197a7ae6a0462
castilloz671 <158379076+castilloz671@users.noreply.github.com>
2024-03-26 18:40:42 -0400

    Verificar si existe un category

--- models/database/categories.js
+++ models/database/categories.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const categorySchema = new mongoose.Schema({
   categoryName: {
@@ -19,24 +19,44 @@ categorySchema.set("toJSON", {
 
 const Category = mongoose.model("Category", categorySchema);
 
-export class CategoriesModel{
+export class CategoriesModel {
   static async getAll() {
     return await Category.find();
   }
 
   static async getById({ id }) {
-    return await Category.findById(id)
+    return await Category.findById(id);
   }
 
   static async createCategory({ input }) {
-    const newCategory = await Category.create(input)
-    return newCategory;
+    const existingCategory = await Category.findOne({
+      categoryName: input.categoryName,
+    });
+
+    if (existingCategory) {
+      return { existingCategory, isExisting: true };
+    }
+    const newCategory = await Category.create(input);
+    return { newCategory, isExisting: false };
   }
 
   static async updateCategory({ id, input }) {
-    const updatedCategory = await Category.findByIdAndUpdate(id, input, { new: true })
+    // Verificar si ya existe otra categoría con el mismo nombre y diferente ID
+    const existingCategory = await Category.findOne({
+      categoryName: input.categoryName,
+      _id: { $ne: id },
+    });
 
-    return updatedCategory ? updatedCategory : false
+    if (existingCategory) {
+      // Retornar algo que indique que ya existe una categoría con ese nombre
+      return { conflict: true, existingCategory };
+    }
+
+    // Si no hay conflicto, procede a actualizar la categoría
+    const updatedCategory = await Category.findByIdAndUpdate(id, input, {
+      new: true,
+    });
+    return { conflict: false, updatedCategory };
   }
 
   static async deletedCategory({ id }) {
@@ -47,5 +67,4 @@ export class CategoriesModel{
       throw error;
     }
   }
-
 }
