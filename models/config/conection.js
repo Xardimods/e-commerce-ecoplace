8ec34a65b824ee9c7f00c3a7c699234c11c3aadf
Enyel Liranzo <xardimodschannel@gmail.com>
2024-03-28 10:23:55 -0400

    update: adding .env variables for security

--- models/config/conection.js
+++ models/config/conection.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv/config'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const conection = process.env.CONECTION_STRING;
 
-export const bbddConection = mongoose.connect(conection)
+export const db = mongoose.connect(conection)
   .then(() => {
     console.log("Connected to the BBDD!");
   })
