408381fc38ffe7c8727dfbc7f359a96ed2f1a2f3
Enyel Liranzo <xardimodschannel@gmail.com>
2024-03-28 12:30:35 -0400

    feat: adding users controllers methods

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,17 @@
 import { UserModel } from "../models/database/users";
 
 export class UserController {
+  static async getUser(req, res) { }
 
+  static async createUser(req, res) { }
+
+  static async updateUser(req, res) { }
+
+  static async deleteUser(req, res) { }
+
+  static async logInUser(req, res) { }
+
+  static async logOutUser(req, res) { }
+
+  static async logAuthAllUser(req, res) { }
 }
