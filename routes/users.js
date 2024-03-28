b4363db712887c808fec4c3a466b8f89f2e0eb5e
Enyel Liranzo <xardimodschannel@gmail.com>
2024-03-28 14:00:12 -0400

    update: changing directories

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,11 @@ import { UserController } from "../controllers/users.js";
 
 export const UserRouter = Router();
 
-UserRouter.get('/me', UserController.getUser);
-
-UserRouter.post('/', UserController.createUser)
+UserRouter.post('/register', UserController.createUser)
 UserRouter.post('/login', UserController.logInUser);
 UserRouter.post('/logout', UserController.logOutUser);
 UserRouter.post('/logout-all', UserController.logAuthAllUser);
 
+UserRouter.get('/me', UserController.getUser);
 UserRouter.patch('/me', UserController.updateUser);
-
 UserRouter.delete('/me', UserController.deleteUser);
