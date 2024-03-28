d4cbf76e838d829968a67dfd60d304a1b1b9cda8
Enyel Liranzo <xardimodschannel@gmail.com>
2024-03-28 13:59:50 -0400

    try: trying to make authentication

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+export const auth = (async (req, res, next) => {
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '')
+    const decode = jwt.verify(token, 'bootcamptalendig')
+    const user = await User.findOne({ _id: decode._id, 'tokens.token': token })
+
+    if (!user) {
+      throw Error()
+    }
+
+    req.token = token
+    req.user = user
+    next();
+  }
+  catch (e) {
+    res.status(401).send({ error: 'Auth Error!' })
+  }
+
+})
