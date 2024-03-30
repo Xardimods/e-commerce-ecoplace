2202fca06a363c82532cc3c2320e2a2f99b60b9f
castilloz671 <158379076+castilloz671@users.noreply.github.com>
2024-03-29 23:23:55 -0400

    Verify auth for categories.

--- routes/categories.js
+++ routes/categories.js
@@ -1,15 +1,14 @@
 import { Router } from 'express';
 import { CategoriesController } from '../controllers/categories.js';
-import auth from '../middleware/auth.js';
 
 export const CategoriesRouter = Router();
 
-CategoriesRouter.get('/', auth, CategoriesController.getAll);
-CategoriesRouter.get('/:id', auth, CategoriesController.getById);
-CategoriesRouter.get('/search/:name', auth, CategoriesController.findCategoryByName);
+CategoriesRouter.get('/', CategoriesController.getAll);
+CategoriesRouter.get('/:id', CategoriesController.getById);
+CategoriesRouter.get('/search/:name', CategoriesController.findCategoryByName);
 
-CategoriesRouter.post('/', auth, CategoriesController.createCategory);
+CategoriesRouter.post('/', CategoriesController.createCategory);
 
-CategoriesRouter.put('/:id', auth, CategoriesController.updateCategory)
+CategoriesRouter.put('/:id', CategoriesController.updateCategory)
 
-CategoriesRouter.delete('/:id', auth, CategoriesController.deleteCategory)
+CategoriesRouter.delete('/:id', CategoriesController.deleteCategory)
