2ca4ff633c3bf82295ae01a9ec8831fdb6dd8111
castilloz671 <158379076+castilloz671@users.noreply.github.com>
2024-04-04 16:23:21 -0400

    Changes firebase.

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,13 @@
 import admin from 'firebase-admin'
 
-const serviceAccount = JSON.parse(Buffer.from(process.env.FIREBASE_ADMIN_SDK_JSON_BASE64, 'base64').toString('ascii'));
-
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+}
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'ecoplaceapiimages.appspot.com'
+  storageBucket: process.env.REACT_APP_storageBucket
 })
 
 const bucket = admin.storage().bucket()
