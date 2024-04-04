93b1540408532291b3cb4a6c4bbb2660dc0807d6
castilloz671 <158379076+castilloz671@users.noreply.github.com>
2024-04-03 20:38:33 -0400

    Fixing cors.

--- middleware/cors.js
+++ middleware/cors.js
@@ -21,5 +21,13 @@ export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => c
 
     return callback(new Error('Not allowed by CORS'))
   },
-  credentials: true
+  credentials: true,
+  // Métodos HTTP permitidos
+  methods: ['GET', 'POST', 'OPTIONS', 'PATCH', 'DELETE'],
+  // Cabeceras permitidas en las solicitudes
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+  // Cabeceras expuestas en las respuestas al cliente
+  exposedHeaders: ['X-Auth-Token', 'Content-Type', 'Accept'],
+  // Configura el tiempo (en segundos) que el resultado de una solicitud preflight puede ser caché
+  maxAge: 86400, // 24 horas
 })
