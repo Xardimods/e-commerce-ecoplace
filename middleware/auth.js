7a3c96eff06434b3b85a9bb6850b8b5b637ad59b
castilloz671 <158379076+castilloz671@users.noreply.github.com>
2024-04-03 23:05:52 -0400

    Adding time to session.

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,7 +10,7 @@ const auth = async (req, res, next) => {
             throw new Error('Authentication token not found')
         }
         
-        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
         
         if (!user) {
