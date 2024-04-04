e2d5cfd193248237b2bc9a13e36a386e781b2473
castilloz671 <158379076+castilloz671@users.noreply.github.com>
2024-04-04 17:43:02 -0400

    Again.

--- models/config/firebase-config.js
+++ models/config/firebase-config.js
@@ -2,7 +2,7 @@ import admin from 'firebase-admin'
 
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  privateKey: process.env.FIREBASE_PRIVATE_KEY,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 }
 admin.initializeApp({
