27451f047ed2cfdeed66b3b066214aeff6b47678
Enyel Liranzo <xardimodschannel@gmail.com>
2024-04-09 04:06:38 -0400

    feat: adding methods for seller in sales controllers

--- controllers/sales.js
+++ controllers/sales.js
@@ -1,19 +1,19 @@
 import { SaleModel } from "../models/database/sales.js";
 
 export class SaleController {
-  static async getAllSales(req, res) {
+  static async getAllSalesForAdmin(req, res) {
     try {
-      const sales = await SaleModel.getAllSales();
+      const sales = await SaleModel.getAllSalesForAdmin();
       res.json(sales);
     } catch (error) {
       res.status(500).json({ message: "Error al obtener las ventas", error });
     }
   }
 
-  static async getSalesBySeller(req, res) {
+  static async getSalesBySellerforAdmin(req, res) {
     const { sellerId } = req.params;
     try {
-      const sales = await SaleModel.getSalesBySeller(sellerId);
+      const sales = await SaleModel.getSalesBySellerForAdmin(sellerId);
       res.json(sales);
     } catch (error) {
       res.status(500).json({ message: error.message });
@@ -21,16 +21,38 @@ export class SaleController {
   }
 
   // Obtener venta por ID de la orden con status Paid
-  static async getSalesByOrderId(req, res) {
+  static async getSalesByOrderIdforAdmin(req, res) {
     const { orderId } = req.params;
     try {
-      const sale = await SaleModel.getSalesByOrderId(orderId);
+      const sale = await SaleModel.getSalesByOrderIdforAdmin(orderId);
       if (!sale) {
         return res.status(404).json({ message: "Sale not found" });
       }
       res.json(sale);
     } catch (error) {
       res.status(500).json({ message: error.message });
+    }
+  }
+
+  // Methods for Sellers
+  static async getAllSalesForSeller(req, res) {
+    try {
+      const sellerId = req.user._id;
+      const sellerSales = await SaleModel.getAllSalesForSeller(sellerId);
+      res.json(sellerSales);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  static async getSalesByOrderIdforSeller(req, res) {
+    try {
+      const { orderId } = req.params;
+      const sellerId = req.user._id;
+      const order = await SaleModel.getSalesByOrderIdforSeller(sellerId, orderId);
+      res.json(order);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
   }
 }
