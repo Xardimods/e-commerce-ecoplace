29468afe6a1709666c82c8982833710ccf6b976a
castilloz671 <158379076+castilloz671@users.noreply.github.com>
2024-04-30 21:11:01 -0400

    Fixing errors.

--- models/database/orders.js
+++ models/database/orders.js
@@ -110,7 +110,6 @@ export class OrderModel {
     try {
       let orders = await Order.find({ customer: userId })
           .sort({ createdAt: -1 })
-          .limit(1)
           .populate('items.product', 'name price')
           .populate('customer', 'name lastname street city country zip paymentDetails');
 
