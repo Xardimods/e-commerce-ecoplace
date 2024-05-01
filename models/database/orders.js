598e064a9ee9600f753118672a53457f96075092
castilloz671 <158379076+castilloz671@users.noreply.github.com>
2024-04-30 20:26:25 -0400

    Editing orders.

--- models/database/orders.js
+++ models/database/orders.js
@@ -1,84 +1,74 @@
-import mongoose from "mongoose";
-import { Cart } from "./carts.js";
+import mongoose from 'mongoose';
+import { Cart } from './carts.js';
 
-const orderItemSchema = new mongoose.Schema(
-  {
-    product: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Product",
-      required: true,
-    },
-    quantity: {
-      type: Number,
-      required: true,
-    },
+const orderItemSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
   },
-  {
-    timestamps: false,
-    _id: false, // Evita la creación de un _id para subdocumentos si no es necesario
-  }
-);
+  quantity: {
+    type: Number,
+    required: true,
+  },
+}, {
+  timestamps: false,
+  _id: false // Evita la creación de un _id para subdocumentos si no es necesario
+});
 
-const paymentDetailsSchema = new mongoose.Schema(
-  {
-    paymentMethodId: String,
-    amountPaid: Number,
-    cardLastFourDigits: String, // Guardar solo los últimos cuatro dígitos
-    cardExpirationDate: String,
-    cardHolderName: String,
-  },
-  {
-    _id: false,
-  }
-);
+const paymentDetailsSchema = new mongoose.Schema({
+  paymentMethodId: String,
+  amountPaid: Number,
+  cardLastFourDigits: String, // Guardar solo los últimos cuatro dígitos
+  cardExpirationDate: String,
+  cardHolderName: String,
+}, {
+  _id: false
+});
 
-const orderSchema = new mongoose.Schema(
-  {
-    items: [orderItemSchema],
-    customer: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    status: {
-      type: String,
-      required: true,
-      enum: ["Pending", "Paid", "Shipped", "Cancelled"],
-      default: null,
-    },
-    trackingNumber: {
-      type: String,
-      default: "",
-    },
-    paymentDetails: paymentDetailsSchema,
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
+const orderSchema = new mongoose.Schema({
+  items: [orderItemSchema],
+  customer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
   },
-  {
-    timestamps: true, // Aprovecha la creación automática de campos createdAt y updatedAt
-  }
-);
+  status: {
+    type: String,
+    required: true,
+    enum: ["Pending", "Paid", "Shipped", "Cancelled"],
+    default: null,
+  },
+  trackingNumber: {
+    type: String,
+    default: "",
+  },
+  paymentDetails: paymentDetailsSchema,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+}, {
+  timestamps: true // Aprovecha la creación automática de campos createdAt y updatedAt
+});
+
 
 const Order = mongoose.model("Order", orderSchema);
 
-export { Order };
+export { Order }
 
 export class OrderModel {
   static async createOrderFromCart(userId, paymentDetails) {
     try {
-      const cart = await Cart.findOne({ User: userId }).populate(
-        "items.product"
-      );
-
+      const cart = await Cart.findOne({ User: userId }).populate('items.product');
+      
       if (!cart || cart.items.length === 0) {
-        throw new Error("No items in cart.");
+        throw new Error('No items in cart.');
       }
 
       // Aquí iría tu lógica para crear la orden basada en los ítems del carrito
       const order = await Order.create({
-        items: cart.items.map((item) => ({
+        items: cart.items.map(item => ({
           product: item.product._id,
           quantity: item.quantity,
         })),
@@ -90,120 +80,107 @@ export class OrderModel {
           cardExpirationDate: paymentDetails.cardExpirationDate || "No Aplica",
           cardHolderName: paymentDetails.cardHolderName || "No Aplica",
         },
-        status: "Paid", // Asume que este estado indica que el pago fue exitoso
+        status: 'Paid', // Asume que este estado indica que el pago fue exitoso
       });
 
       return order;
     } catch (error) {
-      throw new Error("Error creating order from cart: " + error.message);
+      throw new Error('Error creating order from cart: ' + error.message);
     }
   }
 
   static async emptyCart(userId) {
     try {
       const cart = await Cart.findOne({ User: userId });
-
+      
       if (!cart) {
-        throw new Error("Cart not found.");
+        throw new Error('Cart not found.');
       }
 
       cart.items = [];
       await cart.save();
 
-      return { success: true, message: "Cart emptied successfully." };
+      return { success: true, message: 'Cart emptied successfully.' };
     } catch (error) {
-      throw new Error("Error emptying cart: " + error.message);
+      throw new Error('Error emptying cart: ' + error.message);
     }
   }
 
   static async getOrdersByUser(userId) {
     try {
       let orders = await Order.find({ customer: userId })
-        .sort({ createdAt: -1 })
-        .populate("items.product", "name price")
-        .populate(
-          "customer",
-          "name lastname street city country zip paymentDetails"
-        );
+          .sort({ createdAt: -1 })
+          .populate('items.product', 'name price')
+          .populate('customer', 'name lastname street city country zip paymentDetails');
 
-      if (orders.length === 0) {
-        return null; // Manejo en caso de no encontrar órdenes
-      }
+      // Asegurarse de manejar ítems cuyo producto ha sido eliminado
+      orders = orders.map(order => {
+          const orderObject = order.toObject();
 
-      const order = orders[0].toObject(); // Convertir a objeto si hay una orden
+          let total = 0;
+          orderObject.items = orderObject.items.map(item => {
+              // Verifica si el producto existe antes de acceder a sus propiedades
+              const itemTotal = item.product ? item.quantity * item.product.price : 0;
+              total += itemTotal;
+              return {
+                  ...item,
+                  subtotal: itemTotal,
+                  product: item.product ? item.product : { name: "Product deleted", price: 0 }
+              };
+          });
 
-      let total = 0;
-      order.items = order.items.map((item) => {
-        const itemTotal = item.product ? item.quantity * item.product.price : 0;
-        total += itemTotal;
-        return {
-          ...item,
-          subtotal: itemTotal,
-          product: item.product
-            ? item.product
-            : { name: "Product deleted", price: 0 },
-        };
+          orderObject.total = total;
+          return orderObject;
       });
 
-      order.total = total; // Añadir el total calculado al objeto de la orden
-
-      return order; // Devuelve la última orden
+      return orders;
     } catch (error) {
-      console.error("Error fetching orders:", error);
-      throw error;
+        console.error("Error fetching orders:", error);
+        throw error;
     }
   }
 
   static async getOrderById(orderId) {
     try {
-      const order = await Order.findById(orderId)
-        .populate("items.product", "name price")
-        .populate(
-          "customer",
-          "name lastname street city country zip paymentDetails"
-        );
+    const order = await Order.findById(orderId)
+      .populate('items.product', 'name price')
+      .populate('customer', 'name lastname street city country zip paymentDetails');
 
-      // Verifica si la orden existe antes de intentar acceder a sus propiedades
-      if (!order) {
-        throw new Error("Order not found");
-      }
+    // Verifica si la orden existe antes de intentar acceder a sus propiedades
+    if (!order) {
+      throw new Error('Order not found');
+    }
 
-      const total = order.items.reduce(
-        (acc, item) => acc + item.product.price * item.quantity,
-        0
-      );
-      const orderObject = order.toObject();
-      orderObject.total = total; // Añadir el total calculado al objeto de la orden
+    const total = order.items.reduce((acc, item) => acc + (item.product.price * item.quantity), 0);
+    const orderObject = order.toObject(); 
+    orderObject.total = total; // Añadir el total calculado al objeto de la orden
 
-      return orderObject;
+    return orderObject;
     } catch (error) {
-      throw new Error("Error fetching order: " + error.message);
+      throw new Error('Error fetching order: ' + error.message);
     }
   }
 
-  static async getAllOrders() {
-    // Solo funcionara con los Admins
+  static async getAllOrders() {  // Solo funcionara con los Admins
     try {
       const orders = await Order.find()
-        .populate("items.product", "name price quantity")
-        .populate("customer", "name lastname");
+      .populate('items.product', 'name price quantity')
+      .populate('customer', 'name lastname');
       return orders;
     } catch (error) {
-      throw new Error("Error fetching all orders: " + error.message);
+      throw new Error('Error fetching all orders: ' + error.message);
     }
   }
 
   static async updateOrder(orderId, updateData) {
     try {
-      const order = await Order.findByIdAndUpdate(orderId, updateData, {
-        new: true,
-      });
+      const order = await Order.findByIdAndUpdate(orderId, updateData, { new: true });
       if (!order) {
-        throw new Error("Order not found");
+        throw new Error('Order not found');
       }
       return order;
     } catch (error) {
-      throw new Error("Error updating order: " + error.message);
+      throw new Error('Error updating order: ' + error.message);
     }
   }
 }
