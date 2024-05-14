6540f2ac5d6e792a093ec67665dcf5afbe34d369
castilloz671 <158379076+castilloz671@users.noreply.github.com>
2024-05-14 19:34:12 -0400

    Testing.

--- controllers/orders.js
+++ controllers/orders.js
@@ -74,8 +74,8 @@ export class OrderController {
         payment_method_types: ['card'],
         line_items: lineItems,
         mode: 'payment',
-        success_url: 'https://ecoplace.vercel.app/success?session_id={CHECKOUT_SESSION_ID}',
-        cancel_url: 'https://ecoplace.vercel.app/cancel',
+        success_url: 'https://ecoplaceapp.netlify.app/success?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url: 'https://ecoplaceapp.netlify.app/cancel',
       });
 
       res.json({ sessionId: session.id });
